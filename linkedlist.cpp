ec055400391318927c47b6e2e80f2965116ce7cd
aaruzzz <bishanttimalsina14@gmail.com>
2020-03-19 21:06:56 +0545

    Linked List

---
+++ linkedlist.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+using namespace std;
+class node{
+	int data;
+	node* left;
+	node* right;
+}
+void insert(node* root,int data)
+int main(){
+ 	node* root;  //pointer to root node
+	root= NULL;  //setting tree as empty i.e. no children
+}
